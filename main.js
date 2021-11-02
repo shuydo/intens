b9f41952120353e8cd7b30e03e58ffc6b9ee9342
Shuydo <shuydo12@gmail.com>
2021-11-02 03:42:48 +0200

    slider + part responsive

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const swiper = new Swiper(".swiper", {
   loop: true,
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    // nextEl: ".slider-button-next",
-    // prevEl: ".slider-button-prev",
+    // nextEl: ".swiper-button-next",
+    // prevEl: ".swiper-button-prev",
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
 });
